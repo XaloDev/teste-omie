a307d32a11e3eb1d082d90065e7fb71da0f0ff6e
icaro <xalodeveloper@gmail.com>
2022-03-14 04:46:38 -0300

    att

---
+++ README.md
@@ -0,0 +1,2 @@
+Online Web App Link:
+Online API Link: https://teste-omie-dev.herokuapp.com/
--- client/src/environments/constants.ts
+++ client/src/environments/constants.ts
@@ -2,7 +2,7 @@ import {isDevMode} from "@angular/core";
 
 export class Constants{
 
-  public static DB_BASE_URL = isDevMode() ? 'http://localhost:8080/' : 'http://localhost:8080/';
+  public static DB_BASE_URL = isDevMode() ? 'http://localhost:8080/' : 'https://teste-omie-dev.herokuapp.com/';
 
   public static CLIENTES_PATH = Constants.DB_BASE_URL + 'clientes'
   public static CATEGORIAS_PATH = Constants.DB_BASE_URL + 'categorias'
